88ed85a34f6f3f2ccac5e3b33733416889b026c2
kavyangaHA <kavyangahathurusinghe1234@gmail.com>
2026-01-03 14:34:37 +0530

    created Essence cls

---
+++ core/src/main/java/com/mygdx/game/Essence.java
@@ -0,0 +1,14 @@
+package com.mygdx.game;
+
+public class Essence {
+    public int x;
+    public int y;
+    public String type;//ex-"Sun",Water","Shadow"
+
+    public Essence(int x,int y,String type){
+        this.x = x;
+        this.y =y;
+        this.type = type;
+    }
+
+}
