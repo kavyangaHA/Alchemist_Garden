156f3682ad59eaa98f2f5a3ac257b14deffa1d20
kavyangaHA <kavyangahathurusinghe1234@gmail.com>
2026-02-16 23:18:21 +0530

    try to fix a bug

--- core/src/main/java/com/mygdx/game/MyGdxGame.java
+++ core/src/main/java/com/mygdx/game/MyGdxGame.java
@@ -1,34 +1,28 @@
 package com.mygdx.game;
-
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class MyGdxGame extends Game {
-    // We will hold our current screen here
-    private GameScreen gameScreen;
-
+    public BitmapFont font;
     @Override
     public void create() {
-        // Create an instance of our game screen
-        gameScreen = new GameScreen();
-        // Set it as the current screen for the game
-        setScreen(gameScreen);
+        // Set the screen to our new TitleScreen
+        font = new BitmapFont();
+        setScreen(new GameScreen(this));
     }
 
     @Override
     public void render() {
-        // Clear the screen
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.15f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        // This is crucial! It calls the render method of our current screen
-        super.render();
+        super.render(); // This is crucial! It calls the render method of the current screen
     }
 
     @Override
     public void dispose() {
-        // Dispose of the screen when the game closes
-        gameScreen.dispose();
+        super.dispose();
     }
 }
